25d553adb1f3849a156c176380b5acacbf8aa2c0
idfeisal <feisal.warsame@bluewin.ch>
2025-08-27 15:31:32 +0200

    game.js

--- game.js
+++ game.js
@@ -33,7 +33,7 @@ function App() {
     });
 
     try {
-      const response = await fetch("http://localhost:3000/generate-story", {
+      const response = await fetch("http://localhost:3000", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ prompt }),
